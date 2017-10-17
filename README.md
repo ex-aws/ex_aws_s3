4ce4d72219c0cb8de9356278203d8ea780311b75
Ben Wilson <benwilson512@gmail.com>
2017-10-17 09:43:14 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# ExAws.S3
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `ex_aws_s3` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:ex_aws_s3, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/ex_aws_s3](https://hexdocs.pm/ex_aws_s3).
+
---
+++ mix.exs
@@ -0,0 +1,41 @@
+defmodule ExAws.S3.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :ex_aws_s3,
+      version: "0.1.0",
+      elixir: "~> 1.5",
+      elixirc_paths: elixirc_paths(Mix.env),
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_),     do: ["lib",]
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      ex_aws(),
+      {:hackney, ">= 0.0.0", only: [:dev, :test]},
+      {:sweet_xml, ">= 0.0.0", only: [:dev, :test]},
+      {:bypass, "~> 0.7", only: :test},
+    ]
+  end
+
+  defp ex_aws() do
+    case System.get_env("AWS") do
+      "LOCAL" -> {:ex_aws, path: "../ex_aws"}
+      _ -> {:ex_aws, github: "ex-aws/ex_aws", branch: "v2"}
+    end
+  end
+end
