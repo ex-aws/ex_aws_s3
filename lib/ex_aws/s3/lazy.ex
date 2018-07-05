750b72c7d77705e39fc66f7f104c84770c477083
Gary Rennie <gazler@gmail.com>
2018-07-05 17:54:03 +0100

    Fix the S3.Lazy implementation when fetching common_prefixes
    Previously, fetching common_prefixes would return an empty list as the
    contents are empty:
    
        ExAws.S3.list_objects(bucket, delimeter: "/", prefix: "foo/")
        |> ExAws.stream!()
        |> Enum.map(&(&1))
    
    This update prioritizes streaming the common_prefixes if the contents
    are empty.

--- lib/ex_aws/s3/lazy.ex
+++ lib/ex_aws/s3/lazy.ex
@@ -1,6 +1,6 @@
 defmodule ExAws.S3.Lazy do
   @moduledoc false
-  ## Implimentation of the lazy functions surfaced by ExAws.S3.Client
+  ## Implementation of the lazy functions surfaced by ExAws.S3.Client
   def stream_objects!(bucket, opts, config) do
     request_fun = fn fun_opts ->
       ExAws.S3.list_objects(bucket, Keyword.merge(opts, fun_opts))
@@ -13,8 +13,14 @@ defmodule ExAws.S3.Lazy do
 
       {fun, args} -> case fun.(args) do
 
+        results = %{contents: [], common_prefixes: prefixes, is_truncated: "true"} ->
+          {prefixes, {fun, [marker: next_marker(results)]}}
+
         results = %{contents: contents, is_truncated: "true"} ->
           {contents, {fun, [marker: next_marker(results)]}}
+
+        %{common_prefixes: prefixes} when prefixes != [] ->
+          {prefixes, :quit}
 
         %{contents: contents} ->
           {contents, :quit}
