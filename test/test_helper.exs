7dc171832927e0451eb98e86abb05766b9ad5c6d
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2020-06-23 18:13:09 +0900

    Configure access key and secret key with dummy value to make AuthCache work

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,5 @@
+# Configure access key and secret key with dummy value to make AuthCache work.
+System.put_env("AWS_ACCESS_KEY_ID", "dummy")
+System.put_env("AWS_SECRET_ACCESS_KEY", "dummy")
+
 ExUnit.start()
