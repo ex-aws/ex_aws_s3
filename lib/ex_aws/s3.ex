1a7a64b6ff78a765ad13d63a738ea4dfd7511d6e
Daniel Buecherl <emoniuss@gmail.com>
2021-05-19 15:59:48 +0200

    Update s3.ex
    Fix spec return value

--- lib/ex_aws/s3.ex
+++ lib/ex_aws/s3.ex
@@ -432,15 +432,16 @@ defmodule ExAws.S3 do
   ## Example
   ```
   ExAws.S3.put_bucket_versioning(
-    "my-bucket",
-    "<VersioningConfiguration><Status>Enabled</Status></VersioningConfiguration>"
+   "my-bucket",
+   "<VersioningConfiguration><Status>Enabled</Status></VersioningConfiguration>"
   )
   |> ExAws.request
   ```
   """
-  @spec put_bucket_versioning(bucket :: binary, version_config :: binary) :: no_return
+  @spec put_bucket_versioning(bucket :: binary, version_config :: binary) ::
+          ExAws.Operation.S3.t()
   def put_bucket_versioning(bucket, version_config) do
-    content_md5 = :crypto.hash(:md5, version_config) |> Base.encode64
+    content_md5 = :crypto.hash(:md5, version_config) |> Base.encode64()
     headers = %{"content-md5" => content_md5}
     request(:put, bucket, "/", resource: "versioning", body: version_config, headers: headers)
   end
