051fd1ac25ae04b30004d9d3d6dc7d4d3431f4d1
Henry Miao <henry.miao@intelligencebank.com>
2022-08-04 16:48:24 +1000

    list_objects_v2 to use dedicated streaming function

--- lib/ex_aws/s3/lazy.ex
+++ lib/ex_aws/s3/lazy.ex
@@ -27,6 +27,33 @@ defmodule ExAws.S3.Lazy do
     )
   end
 
+  def stream_objects_v2!(bucket, opts, config) do
+    request_fun = fn fun_opts ->
+      ExAws.S3.list_objects_v2(bucket, Keyword.merge(opts, fun_opts))
+      |> ExAws.request!(config)
+      |> Map.get(:body)
+    end
+
+    Stream.resource(
+      fn -> {request_fun, []} end,
+      fn
+        :quit ->
+          {:halt, nil}
+
+        {fun, args} ->
+          case fun.(args) do
+            results = %{is_truncated: "true"} ->
+              {add_results(results, opts),
+               {fun, [continuation_token: results[:next_continuation_token]]}}
+
+            results ->
+              {add_results(results, opts), :quit}
+          end
+      end,
+      & &1
+    )
+  end
+
   def add_results(results, opts) do
     case Keyword.get(opts, :stream_prefixes, nil) do
       nil -> results.contents
